7510cb4d34ad9fadcdcd6f769376c8d40b869d83
Biking Wang <baiqin.fubotv@gmail.com>
2024-11-01 09:59:57 -0400

    Add initial multi-platform project.

---
+++ plot/build.gradle.kts
@@ -0,0 +1,82 @@
+import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.vanniktech.mavenPublish)
+}
+
+group = "io.github.kotlin"
+version = "0.1.0"
+
+kotlin {
+    jvm()
+    androidTarget {
+        publishLibraryVariants("release")
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
+        }
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    linuxX64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.composexy"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates(group.toString(), "library", version.toString())
+
+    pom {
+        name = "composexy"
+        description = "A compose multi-platform library."
+        inceptionYear = "2024"
+        url = "https://github.com/composexy/composexy/"
+        licenses {
+            license {
+                name = ""
+                url = ""
+                distribution = ""
+            }
+        }
+        developers {
+            developer {
+                id = ""
+                name = ""
+                url = ""
+            }
+        }
+        scm {
+            url = ""
+            connection = ""
+            developerConnection = ""
+        }
+    }
+}
